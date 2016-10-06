25b8e8944f6c7538b534967e1c24a7d8f6e6912b
Jasobanta Narayan Das <j100775@gmail.com>
2016-10-06 19:52:12 +0530

    put files

--- webservice3/app/Controller/UsersController.php
+++ webservice3/app/Controller/UsersController.php
@@ -61,7 +61,7 @@ class UsersController extends AppController {
             $this->set('response_body', array('message' => 'Please Enter Password', 'code' => 1));
         }
 
-        $this->render('/users/request_response');    // display request response
+        $this->render('/Users/request_response');    // display request response
     }
 
 // new user will be register
